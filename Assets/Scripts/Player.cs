fad088612733506fb59b3e3ec6d414368af27e56
pgzer0 <passiongamezero@gmail.com>
2024-07-06 11:31:01 -0500

    player movement complete. pre laser work

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    private float xBound = 11.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x >= xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, 0);
+        }
+    }
+}
