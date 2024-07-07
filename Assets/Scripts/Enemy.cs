e08c9935a07e73691f184cc9bb05335686ece960
pgzer0 <passiongamezero@gmail.com>
2024-07-07 08:35:41 -0500

    enemy setup complete. pre spawn manager setup.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float _speed = 4f;
+
+   
+    
+    void Start()
+    {
+     
+    }
+
+    
+    void Update()
+    {
+        float _randomX = Random.Range(-9.4f, 9.4f);
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if(transform.position.y < -6)
+        {
+            transform.position = new Vector3(_randomX, 8, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+
+            if(player != null)
+            {
+                player.Damage();
+            }
+            Destroy(gameObject);
+        }
+        else if(other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,26 +7,28 @@ public class Player : MonoBehaviour
     [SerializeField] float speed = 5f;
     private float xBound = 11.3f;
     private Vector3 _offset = new Vector3(-0, 1f, 0);
+
     [SerializeField] float _fireRate = 0.2f;
     float _canFire = -1f;
 
+    [SerializeField] int _lives = 3;
+
     [SerializeField] GameObject _laserPrefab;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
      CalculateMovement();
 
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
-            _canFire = Time.time + _fireRate;
-            Instantiate(_laserPrefab, transform.position + _offset, Quaternion.identity);
+          FireLaser();
         }
 
     }
@@ -48,6 +50,22 @@ public class Player : MonoBehaviour
         else if (transform.position.x <= -xBound)
         {
             transform.position = new Vector3(xBound, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + _offset, Quaternion.identity);
+    }
+
+    public void Damage()
+    {
+        _lives--;
+
+        if(_lives < 1)
+        {
+            Destroy(gameObject);
         }
     }
 }
